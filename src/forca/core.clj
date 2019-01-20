bfdc9097d0f45373df0dbedfdc5d3f39901693da
Rodrigo Oliveira <rodrigodosanjosoliveira@gmail.com>
2019-01-20 11:09:40 -0200

    First commit

---
+++ src/forca/core.clj
@@ -0,0 +1,40 @@
+(ns forca.core
+  (:gen-class))
+
+
+(def total-de-vidas 6)  
+
+(defn ganhou []  (print "Você ganhou!"))
+
+(defn perdeu []  (print "Você perdeu."))
+
+(defn le-letra! [] (read-line))
+
+(defn acertou? [chute palavra] (.contains palavra chute))
+
+(defn letras-faltantes [palavra acertos]
+  (remove (fn [letra] (contains? acertos (str letra))) palavra)
+)
+
+(defn acertou-a-palavra-toda? [palavra acertos]
+  (empty? (letras-faltantes palavra acertos ))
+)
+
+(defn jogo [vidas palavra acertos]
+    (cond
+        (= vidas 0) (perdeu)
+        (acertou-a-palavra-toda? palavra acertos) (ganhou)
+        :else   
+        (let [chute (le-letra!)]
+            (if (acertou? chute palavra)
+                (do
+                    (println "Acertou a letra!")
+                    (recur vidas palavra (conj acertos chute)))
+                (do
+                    (println "Errou a letra! Perdeu vida!")
+                    (recur (dec vidas) palavra acertos))))))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
