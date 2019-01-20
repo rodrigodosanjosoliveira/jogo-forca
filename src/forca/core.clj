df7dd9934497101cc7d7dbe0e7c0e70a00a7e9f0
Rodrigo Oliveira <rodrigodosanjosoliveira@gmail.com>
2019-01-20 14:02:10 -0200

    packaging app

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -4,6 +4,8 @@
 
 (def total-de-vidas 6)  
 
+(def palavra-secreta "MELANCIA")
+
 (defn ganhou []  (print "Você ganhou!"))
 
 (defn perdeu []  (print "Você perdeu."))
@@ -13,12 +15,10 @@
 (defn acertou? [chute palavra] (.contains palavra chute))
 
 (defn letras-faltantes [palavra acertos]
-  (remove (fn [letra] (contains? acertos (str letra))) palavra)
-)
+  (remove (fn [letra] (contains? acertos (str letra))) palavra))
 
 (defn acertou-a-palavra-toda? [palavra acertos]
-  (empty? (letras-faltantes palavra acertos ))
-)
+  (empty? (letras-faltantes palavra acertos )))
 
 (defn imprime-forca [vidas palavra acertos]
   (println "Vidas " vidas)
@@ -43,7 +43,7 @@
                     (println "Errou a letra! Perdeu vida!")
                     (recur (dec vidas) palavra acertos))))))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn comeca-o-jogo [] (jogo total-de-vidas palavra-secreta #{}))
+
+(defn -main [& args]
+  (comeca-o-jogo))
